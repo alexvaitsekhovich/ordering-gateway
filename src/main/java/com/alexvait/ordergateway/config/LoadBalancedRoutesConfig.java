040a27b41e964d10d2ce195d1ae9729bbbb28a18
alexvaitsekhovich <alex.vaitsekhovich@gmail.com>
2020-08-03 00:12:12 +0200

    first commit

---
+++ src/main/java/com/alexvait/ordergateway/config/LoadBalancedRoutesConfig.java
@@ -0,0 +1,30 @@
+package com.alexvait.ordergateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class LoadBalancedRoutesConfig {
+
+    @Bean
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/api/v1/orders*", "/api/v1/orders/*")
+                        .filters(f -> f.circuitBreaker(
+                                c -> c.setName("ordersCircuitBreaker")
+                                        .setFallbackUri("forward:/orders-failover")
+                                        .setRouteId("orders-failover")
+                        ))
+                        .uri("lb://order-api")
+                        .id("order-api")
+                )
+                .route(r -> r.path("/orders-failover/**")
+                        .uri("lb://ms-failover")
+                        .id("orders-failover-service")
+                )
+                .build();
+    }
+
+}
